5a51a25c65f0ed305ddfe09fddac5fa1255ae5fb
NawafIT <nwef918@gmail.com>
2022-08-02 09:19:24 +0300

    new update

---
+++ app/src/main/java/com/example/story/SplachActivity.kt
@@ -0,0 +1,25 @@
+package com.example.story
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplachActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splach)
+        moveToAnotherActivity()
+    }
+
+    private fun moveToAnotherActivity(){
+        // هذا الكود يقوم بتأخير الانتقال من واجهة الى واجهة اخرى
+        Handler(Looper.myLooper()!!).postDelayed({
+            finish() // هذه الدالة تستخدم لحذف واجهة التحميل بعد ان تطهر لمرة واحدة
+            val i: Intent = Intent(this,Login::class.java)
+            startActivity(i)
+        } ,2400)
+
+    }
+}
